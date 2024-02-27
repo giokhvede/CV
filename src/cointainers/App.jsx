0ee8ff58df2ab3aaa022bda1d4afc026aa9979aa
giokhvede <148794939+giokhvede@users.noreply.github.com>
2024-02-27 21:20:33 +0400

    add routes for auth and homescreen

--- src/cointainers/App.jsx
+++ src/cointainers/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { HomeScreen, Authentication } from "../pages";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Suspense fallback={<div>Loading ...</div>}>
+      <Routes>
+        <Route path="/*" element={<HomeScreen />} />
+        <Route path="/auth" element={<Authentication />} />
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default App;
