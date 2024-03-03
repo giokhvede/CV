491848e361c88e36dfa1ecb2b00c10faf44320df
giokhvede <148794939+giokhvede@users.noreply.github.com>
2024-03-03 19:17:22 +0400

    ..

--- src/cointainers/Footer.jsx
+++ src/cointainers/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="w-full flex items-center justify-between border-t border-gray-300">
       <div className="flex items-center justify-center gap-3 py-3">
         <img src={Logo} alt="Logo" className="w-8 h-auto object-contain" />
-        <p>Expressume</p>
+        <p>Resume</p>
       </div>
       <div className="flex items-center justify-center gap-6">
         <Link to={"/"} className="text-blue-700 text-sm">
